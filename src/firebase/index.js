9e2499736de14d4e82ce506e64aa2d49548adb7a
Ryell Martinez <ryell.martinez@student.tdsb.on.ca>
2024-12-10 11:30:34 -0500

    added firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCfZpbYamm8sKtJ_zjeajnjAFpFQWC0qsc",
+    authDomain: "summative-9a0b1.firebaseapp.com",
+    projectId: "summative-9a0b1",
+    storageBucket: "summative-9a0b1.firebasestorage.app",
+    messagingSenderId: "771000646814",
+    appId: "1:771000646814:web:9fcab7974e55c53fe4cf8f"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
